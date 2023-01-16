4becbde88f302c1fe82a00c2676c655825d2c007
IanCode <ian@loopie.io>
2023-01-16 07:55:17 -0800

    disable flyout

--- client/frontend/src/components/main.js
+++ client/frontend/src/components/main.js
@@ -73,9 +73,7 @@ export default class Main extends React.Component {
                 
                 {gameResultModal}
                     <div className="main__grid__container">
-                    
-
-                    <div className={this.flyoutName}>
+                    {/* <div className={this.flyoutName}>
                         <div className='flyout__row one'></div>
                         <div className='flyout__row two'>this is a flyout</div>
                         <div className='flyout__row three'>it doesn't do much</div>
@@ -86,51 +84,11 @@ export default class Main extends React.Component {
                             <a href='https://github.com/IanCode/wsim' className='cursorpointer'>project repository</a>
                         </div>
                         <div className='flyout__row eight'></div>
-                    </div>
+                    </div> */}
                     <div className="game__container">
                         <div className={boardName}>
                             {renderedBoard}
                         </div>
-                        {/* <Board size="4" /> */}
-                        {/* <Keyboard appState={this.state.appState}/> */}
-                        {/* <div className='keyboard__grid__container'>
-                            <div className='keyboard__row one'>
-                                <div className='keyboard__button one' onClick={() => this.keyPressed("q")}>Q</div>
-                                <div className='keyboard__button two' onClick={() => this.keyPressed("w")}>W</div>
-                                <div className='keyboard__button three' onClick={() => this.keyPressed("e")}>E</div>
-                                <div className='keyboard__button four' onClick={() => this.keyPressed("r")}>R</div>
-                                <div className='keyboard__button five' onClick={() => this.keyPressed("t")}>T</div>
-                                <div className='keyboard__button six' onClick={() => this.keyPressed("y")}>Y</div>
-                                <div className='keyboard__button seven' onClick={() => this.keyPressed("u")}>U</div>
-                                <div className='keyboard__button eight' onClick={() => this.keyPressed("i")}>I</div>
-                                <div className='keyboard__button nine' onClick={() => this.keyPressed("o")}>O</div>
-                                <div className='keyboard__button ten' onClick={() => this.keyPressed("p")}>P</div>
-                            </div>
-                            <div className='keyboard__row two'>
-                                <div className='keyboard__button one blank'></div>
-                                <div className='keyboard__button two' onClick={() => this.keyPressed("a")}>A</div>
-                                <div className='keyboard__button three' onClick={() => this.keyPressed("s")}>S</div>
-                                <div className='keyboard__button four' onClick={() => this.keyPressed("d")}>D</div>
-                                <div className='keyboard__button five' onClick={() => this.keyPressed("f")}>F</div>
-                                <div className='keyboard__button six' onClick={() => this.keyPressed("g")}>G</div>
-                                <div className='keyboard__button seven' onClick={() => this.keyPressed("h")}>H</div>
-                                <div className='keyboard__button eight' onClick={() => this.keyPressed("j")}>J</div>
-                                <div className='keyboard__button nine' onClick={() => this.keyPressed("k")}>K</div>
-                                <div className='keyboard__button ten' onClick={() => this.keyPressed("l")}>L</div>
-                                <div className='keyboard__button eleven blank'></div>
-                            </div>
-                            <div className='keyboard__row three'>
-                                <div className='keyboard__button one' onClick={() => this.keyPressed("enter")}>ENTER</div>
-                                <div className='keyboard__button two' onClick={() => this.keyPressed("z")}>Z</div>
-                                <div className='keyboard__button three' onClick={() => this.keyPressed("x")}>X</div>
-                                <div className='keyboard__button four' onClick={() => this.keyPressed("c")}>C</div>
-                                <div className='keyboard__button five' onClick={() => this.keyPressed("v")}>V</div>
-                                <div className='keyboard__button six' onClick={() => this.keyPressed("b")}>B</div>
-                                <div className='keyboard__button seven' onClick={() => this.keyPressed("n")}>N</div>
-                                <div className='keyboard__button eight' onClick={() => this.keyPressed("m")}>M</div>
-                                <div className='keyboard__button nine' onClick={() => this.keyPressed("delete")}>DELETE</div>
-                            </div>
-                        </div> */}
                     </div>
                     <div className='keyboard__grid__container'>
                             <div className='keyboard__row one'>
@@ -170,16 +128,6 @@ export default class Main extends React.Component {
                                 <div className='keyboard__button nine' onClick={() => this.keyPressed("delete")}>DELETE</div>
                             </div>
                         </div>
-                    {/* <NameForm appState={this.state.appState}/> */}
-                    {/* <div className='test__input__grid__container'>
-                        <form onSubmit={this.handleSubmit}>
-                            <label>
-                                Name:
-                                <input type="text" value={this.state.value} onChange={this.handleChange} />
-                            </label>
-                            <input type="submit" value="Submit" />
-                        </form>
-                    </div> */}
                 </div>
                 </div>
             </div>
